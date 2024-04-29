3e7af76f6c3f9d90688304ccb09033e3f774e615
BearCubsLOL <student.187827@worcesterschools.net>
2024-04-29 18:30:54 -0400

    TIC TAC TOE DONE

--- Tic-Tac-Toe/Program.cs
+++ Tic-Tac-Toe/Program.cs
@@ -1,4 +1,4 @@
-﻿string[] board = {  "   a     b     c  ",
+﻿string[] board = {  "\n\n   a     b     c  ",
                     "      |     |     ",
                     "1  -  |  -  |  -  ",
                     " _____|_____|_____",
@@ -13,21 +13,19 @@ void Main()
         PrintBoard();
         Choice();
     }
-void PlaceX(int x, int y)
+void PlaceX(int x, int y, string pos)
     {
         string mid = board[x];
 
-                mid = mid.Remove(y,1).Insert(y, "x");
+        mid = mid.Remove(y,1).Insert(y, pos);
 
-                board[x] = mid;
+        board[x] = mid;
 
-                PrintBoard();
+        PrintBoard();
     }
 void PrintBoard() 
     {
         Console.ForegroundColor = ConsoleColor.DarkGreen;
-
-        Console.WriteLine("\n\n\n\n");
                             
         foreach (string line in board)
             {
@@ -37,52 +35,169 @@ void PrintBoard()
         Console.ResetColor();
     }
 
-void Choice()
+void CheckWin(int pos1, int pos11, int pos2, int pos22, int pos3, int pos33)
     {
-        Console.WriteLine("\n\nWhere do you want to play?");
-        string userChoice = Console.ReadLine();
+        string one = board[pos1];
+        string two = board[pos2];
+        string three = board[pos3];
+        if (one[pos11].ToString() == "x" && two[pos22].ToString() == "x" && three[pos33].ToString() == "x")
+            {
+                Console.WriteLine("You Win!!!");
+                Environment.Exit(1);
+            }
+        else if (one[pos11].ToString() == "y" && two[pos22].ToString() == "y" && three[pos33].ToString() == "y")
+            {
+                Console.WriteLine("You Lose!!!");
+                Environment.Exit(1);
+            }
+    }
 
-        if (userChoice.ToLower() == "a1" || userChoice.ToLower() == "1a")
+void CheckAll()
+    {
+        CheckWin(2, 3, 2, 9, 2, 15);
+        CheckWin(5, 3, 5, 9, 5, 15);
+        CheckWin(8, 3, 8, 9, 8, 15);
+        CheckWin(2, 3, 5, 3, 8, 3);
+        CheckWin(2, 9, 5, 9, 8, 9);
+        CheckWin(2, 15, 5, 15, 8, 15);
+        CheckWin(2, 3, 5, 9, 8, 15);
+        CheckWin(2, 15, 5, 9, 8, 3);
+    }
+
+void PlaceCheck(int x, int y, string symbol)
+    {
+        string mid = board[x];
+
+        if (mid[y].ToString() == "-") 
             {
-                PlaceX(2, 3);
+                PlaceX(x, y, symbol);
             }
-        else if (userChoice == "a2" || userChoice == "2a")
+        else if (mid[y].ToString() == "x" || mid[y].ToString() == "")
             {
-                PlaceX(2, 9);
-            }
-        else if (userChoice == "a3" || userChoice == "3a")
-            {
-                PlaceX(2, 15);
-            }
-        else if (userChoice == "b1" || userChoice == "1b")
-            {
-                PlaceX(5, 3);
-            }
-        else if (userChoice == "b2" || userChoice == "2b")
-            {
-                PlaceX(5, 9);
-            }
-        else if (userChoice == "b3" || userChoice == "3b")
-            {
-                PlaceX(5, 15);
-            }
-        else if (userChoice == "c1" || userChoice == "1c")
-            {
-                PlaceX(8, 3);
-            }
-        else if (userChoice == "c2" || userChoice == "2c")
-            {
-                PlaceX(8, 9);
-            }
-        else if (userChoice == "c3" || userChoice == "3c")
-            {
-                PlaceX(8, 15);
-            }
-        else
-            {
-                Console.WriteLine("\n\n\n\nInvalid Response");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("\n\nSomeone Already Played that square");
+                Console.ResetColor();
                 Main();
             }
     }
+
+void PlaceCheckAi(int x, int y, string symbol)
+    {
+        string mid = board[x];
+
+        if (mid[y].ToString() == "-") 
+            {
+                PlaceX(x, y, symbol);
+            }
+        else if (mid[y].ToString() == "x" || mid[y].ToString() == "")
+            {
+                PlaceCheckAi(x, y, symbol);
+            }
+    }
+
+void Choice()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("\n\nWhere do you want to play?");
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        string userChoice = Console.ReadLine();
+        Console.ResetColor();
+        
+
+        if (userChoice.ToString().ToLower() == "a1" || userChoice.ToString().ToLower() == "1a")
+            {
+                PlaceCheck(2, 3, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "b1" || userChoice.ToString().ToLower() == "1b")
+            {
+                PlaceCheck(2, 9, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "c1" || userChoice.ToString().ToLower() == "1c")
+            {
+                PlaceCheck(2, 15, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "a2" || userChoice.ToString().ToLower() == "2a")
+            {
+                PlaceCheck(5, 3, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "b2" || userChoice.ToString().ToLower() == "2b")
+            {
+                PlaceCheck(5, 9, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "c2" || userChoice.ToString().ToLower() == "2c")
+            {
+                PlaceCheck(5, 15, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "a3" || userChoice.ToString().ToLower() == "3a")
+            {
+                PlaceCheck(8, 3, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "b3" || userChoice.ToString().ToLower() == "3b")
+            {
+                PlaceCheck(8, 9, "x");
+            }
+        else if (userChoice.ToString().ToLower() == "c3" || userChoice.ToString().ToLower() == "3c")
+            {
+                PlaceCheck(8, 15, "x");
+            }
+        else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("\n\nInvalid Response");
+                Console.ResetColor();
+                Main();
+            };
+        CheckAll();
+        Ai();
+    }
+
+void Ai()
+    {
+        Random rndspot = new Random();
+        int spot = rndspot.Next(1, 10);
+        if (spot == 1)
+            {
+                PlaceCheck(2, 3, "o");
+            }
+        else if (spot == 2)
+            {
+                PlaceCheck(2, 9, "o");
+            }
+        else if (spot == 3)
+            {
+                PlaceCheck(2, 15, "o");
+            }
+        else if (spot == 4)
+            {
+                PlaceCheck(5, 3, "o");
+            }
+         else if (spot == 5)
+            {
+                PlaceCheck(5, 9, "o");
+            }
+         else if (spot == 6)
+            {
+                PlaceCheck(5, 15, "o");
+            }
+         else if (spot == 7)
+            {
+                PlaceCheck(8, 3, "o");
+            }
+        else if (spot == 8)
+            {
+                PlaceCheck(8, 9, "o");
+            }
+        else if (spot == 9)
+            {
+                PlaceCheck(8, 15, "o");
+            }
+        else
+            {
+                Console.WriteLine("There was an error");
+            }   
+        CheckAll();
+        Choice();
+            }
 
 Main();
